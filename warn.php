8954f537a17a2843b07de52c9f9866e1eba49095
TheH4 <129499935+TheH4@users.noreply.github.com>
2023-03-31 15:04:28 +0530

    Create warn.php
    This file displays a simple message to the user letting them know that their login was unsuccessful. You can customize the HTML and message as needed for your application.

---
+++ warn.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Login Warning</title>
+</head>
+<body>
+  <h1>Login Warning</h1>
+  <p>Your login credentials were incorrect. Please try again.</p>
+</body>
+</html>
